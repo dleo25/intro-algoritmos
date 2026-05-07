dd402a04bd13107e7277b67f61acb69711ec6028
Leonardo Diaz <dleo22225@gmail.com>
2026-05-07 16:14:02 -0300

    //jueves

--- Fecha.java
+++ Fecha.java
@@ -7,24 +7,48 @@
  */
 public class Fecha
 {
-    private int dia;
-    private int mes;
-    private int año;
+    private int ddia;
+    private int mmes;
+    private int aanho;
     
     public Fecha(int dia, int mes, int anho) {
-        assert año >= 1582;
-        assert dia <= 31;
-        assert mes <= 12;
+        assert esValida() == true : "dont be...";
 
-        dia = dia;
-        mes = mes;
-        anho = anho;
+        ddia = dia;
+        mmes = mes;
+        aanho = anho;
         
         
     }
+        
+        public boolean esBisiesto() {
+        if (aanho % 4 != 0) {
+            return false;
+        }   else if (aanho % 100 != 0) {
+            return true;
+        }   else if (aanho % 400 == 0) {
+            return true;
+        }   else {
+            return false;
+            }
+    }
+    public int cantAnho(){
+        if (esBisiesto() == true){
+            return 366;
+        }
+        else {
+            return 365;
+        }
+    }
+    public boolean esValida(){
+        
+        if( aanho >= 1582 && ddia <= 31 && mmes <= 12)
+        {
+            return true;
+        }
+        else{
+            return false;
+            }
+        }
+    }
     
-    
-    
-    
-    
-}
